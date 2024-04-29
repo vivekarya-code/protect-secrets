4f590eb352be6e8f38c1244a6e735fb6d68d9f35
vivek-arya-create <60670600+vivekarya-code@users.noreply.github.com>
2024-04-29 13:44:20 -0400

    Add files via upload

---
+++ protectsecrets.go
@@ -0,0 +1,79 @@
+package protectsecrets
+
+//link1  - https://astaxie.gitbooks.io/build-web-application-with-golang/content/en/09.6.html
+//link2 - https://www.thepolyglotdeveloper.com/2018/02/encrypt-decrypt-data-golang-application-crypto-packages/
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/md5"
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+)
+
+func createHash(key string) string {
+	hasher := md5.New()
+	hasher.Write([]byte(key))
+	return hex.EncodeToString(hasher.Sum(nil))
+}
+
+func encrypt(data []byte, passphrase string) []byte {
+	block, _ := aes.NewCipher([]byte(createHash(passphrase)))
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err.Error())
+	}
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		panic(err.Error())
+	}
+	ciphertext := gcm.Seal(nonce, nonce, data, nil)
+	return ciphertext
+}
+
+// Decrypt function for using in apps
+func Decrypt(data []byte, passphrase string) []byte {
+	key := []byte(createHash(passphrase))
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err.Error())
+	}
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err.Error())
+	}
+	nonceSize := gcm.NonceSize()
+	nonce, ciphertext := data[:nonceSize], data[nonceSize:]
+	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		panic(err.Error())
+	}
+	return plaintext
+}
+
+func encryptFile(filename string, data []byte, passphrase string) {
+	f, _ := os.Create(filename)
+	defer f.Close()
+	f.Write(encrypt(data, passphrase))
+}
+
+// DecryptFile func for using in apps
+func DecryptFile(filename string, passphrase string) []byte {
+	data, _ := ioutil.ReadFile(filename)
+	return Decrypt(data, passphrase)
+}
+
+func main() {
+	fmt.Println("Starting the application...")
+
+	ciphertext := encrypt([]byte("actual password goes here"), "<shh, it's a secret>")
+	fmt.Printf("Encrypted password: %x \n", ciphertext)
+
+	plaintext := Decrypt(ciphertext, "i told you it's a secret")
+	fmt.Printf("Decrypted password: %s\n", plaintext)
+
+}
